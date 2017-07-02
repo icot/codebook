53adc29ec7604c09c6fb0b5ba70bf4bf31d1bc95
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2017-07-02 22:28:28 +0200

    Load and apply mapping

--- stage1/stage1.go
+++ stage1/stage1.go
@@ -7,6 +7,15 @@ import (
 	"strings"
 )
 
+func prettyPrint(input interface{}) {
+	// Display Frequency Table
+	b, err := json.MarshalIndent(input, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Print(string(b))
+}
+
 func freqTable(input string) {
 	table := map[string]float32{}
 	for _, letter := range input {
@@ -21,20 +30,38 @@ func freqTable(input string) {
 	}
 	// Divide by total length
 	for k, v := range table {
-		table[k] = v / float32(len(input))
+		table[k] = 100 * (v / float32(len(input)))
 	}
 
-	// Display Frequency Table
-	b, err := json.MarshalIndent(table, "", "  ")
+	prettyPrint(table)
+}
+
+func loadMapping() map[string]string {
+	raw, _ := ioutil.ReadFile("mapping.json")
+
+	var mapping map[string]string
+
+	err := json.Unmarshal(raw, &mapping)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("Error unmarshalling: ", err)
 	}
-	fmt.Print(string(b))
+
+	return mapping
+}
+
+func applyMapping(input string, mapping map[string]string) {
+	output := make([]string, len(input))
+	for pos, letter := range input {
+		output[pos] = mapping[string(letter)]
+		//fmt.Printf("%d: %s, %s\n", pos, letter, output[pos])
+	}
+	fmt.Printf("\nPlaintext:\n%s\n", strings.Join(output, ""))
 }
 
 func main() {
 	raw, _ := ioutil.ReadFile("cipher.text")
 	cipher := strings.Trim(string(raw), "\n")
-	fmt.Println(string(cipher))
-	freqTable(cipher)
+	mapping := loadMapping()
+	fmt.Printf("Cipher:\n%s\n", string(cipher))
+	applyMapping(cipher, mapping)
 }
