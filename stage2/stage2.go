f63e868efb05ae8dead9fb4e0e8d96db5f6e8095
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2017-07-02 13:42:25 +0200

    State 2 solved
    FABER EST SUAE QUISQUE FORTUNAE APPIUS CLAUDIUS CAECUS DICTUM ARCANUM EST NEUTRON
    
    Each man is the architect of his own fortune.
    Appius Claudius the Blind said: the secret word is neutron.

--- stage2/stage2.go
+++ stage2/stage2.go
@@ -6,8 +6,31 @@ import (
 	"strings"
 )
 
+func caesar(input string, shift int) string {
+
+	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	L := len(alphabet)
+
+	output := make([]byte, len(input))
+
+	for i := 0; i < len(input); i++ {
+		if input[i] != 0x20 {
+			pos := (strings.IndexByte(alphabet, input[i]) + shift) % L
+			output[i] = alphabet[pos]
+		} else {
+			output[i] = 0x20
+		}
+	}
+	return string(output)
+}
+
 func main() {
 	raw, _ := ioutil.ReadFile("cipher.text")
 	cipher := strings.Trim(string(raw), "\n")
-	fmt.Println(string(cipher))
+	chars := []byte(cipher)
+	fmt.Println("Original: ", string(chars))
+	//cipher = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for shift := 0; shift <= 26; shift++ {
+		fmt.Printf("Shift: %d\tResult: %s\n", shift, caesar(cipher, shift))
+	}
 }
